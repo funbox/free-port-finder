d5f5b3ee5d54b9b9c2223fe8ac1512428dd5d212
Dmitry Shvalyov <dmitry@shvalyov.ru>
2017-12-20 11:24:38 +0300

    Add isPortFree and findFreePost funcs

---
+++ index.js
@@ -0,0 +1,38 @@
+const net = require('net');
+
+const isPortFree = port => new Promise(
+  resolve => {
+    const server = net.createServer();
+
+    server.once('error', err => {
+      if (err.code === 'EADDRINUSE') {
+        resolve(false);
+      } else {
+        throw err;
+      }
+    });
+
+    server.once('listening', () => {
+      server.once('close', () => {
+        resolve(true);
+      });
+      server.close();
+    });
+
+    server.listen(port, '0.0.0.0');
+  },
+);
+
+const findFreePort = port => isPortFree(port)
+  .then(isFree => {
+    if (isFree) {
+      return port;
+    }
+
+    return findFreePort(port + 1);
+  });
+
+module.exports = {
+  findFreePort: port => findFreePort(port),
+  isPortFree: port => isPortFree(port),
+};
