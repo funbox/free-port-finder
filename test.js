205edaa87bc68fa7b9c4acbc454594b23780622e
n.andreychenko <natandreychenko@gmail.com>
2020-07-24 14:01:52 +0300

    Close server after test assertion

--- test.js
+++ test.js
@@ -1,81 +1,101 @@
 const net = require('net');
 const test = require('ava');
-const pify = require('pify');
 
 const { findFreePort, isPortFree } = require('.');
 
-test('should return the passed port value', async (t) => {
+const requestedPort = 3010;
+const requestedHost = '127.0.0.1';
+const defaultHost = '0.0.0.0';
+
+const wrapper = (t, run) => new Promise(resolve => run(t, resolve));
+
+test.serial('should return the passed port value', async (t) => {
   const port = await findFreePort(3000);
 
   t.is(port, 3000);
 });
 
-test('should throw a port searching error when finding port outside the available range', async (t) => {
+test.serial('should throw a port searching error when finding port outside the available range', async (t) => {
   const port = await t.throwsAsync(() => findFreePort(65536));
 
   t.true(port.message.includes('should be >= 0 and < 65536. Received 65536.'));
 });
 
-test('should return `true` when available port is chosen', async (t) => {
+test.serial('should return `true` when available port is chosen', async (t) => {
   const port = await isPortFree(3007);
 
   t.is(port, true);
 });
 
-test('should return `false` when occupied port is chosen', async (t) => {
-  const needfulPort = 3010;
-
-  const server = net.createServer();
-  await pify(server.listen.bind(server))(needfulPort, '0.0.0.0');
-
-  const port = await isPortFree(needfulPort);
-
-  t.is(port, false);
-});
-
-test('should throw a port searching error when checking port outside the available range', async (t) => {
+test.serial('should throw a port searching error when checking port outside the available range', async (t) => {
   const port = await t.throwsAsync(() => isPortFree(65536));
 
   t.true(port.message.includes('should be >= 0 and < 65536. Received 65536.'));
 });
 
-test('should return first available port', async (t) => {
-  const needfulPort = 3005;
-
-  const server = net.createServer();
-  await pify(server.listen.bind(server))(needfulPort, '0.0.0.0');
-
-  const port = await findFreePort(needfulPort);
-
-  t.not(port, needfulPort);
-});
-
-test('should return the passed port value of the requested host', async (t) => {
+test.serial('should return the passed port value of the requested host', async (t) => {
   const port = await findFreePort(3000, '127.0.0.1');
 
   t.is(port, 3000);
 });
 
-test('should return `false` when occupied port of a requested host is chosen', async (t) => {
-  const requestedHost = '127.0.0.1';
-  const requestedPort = 3010;
-
+test.serial('should return `false` when occupied port is chosen', wrapper, (t, done) => {
   const server = net.createServer();
-  await pify(server.listen.bind(server))(requestedPort, requestedHost);
 
-  const port = await isPortFree(requestedPort, requestedHost);
+  server.once('listening', async () => {
+    server.once('close', done);
 
-  t.is(port, false);
+    const port = await isPortFree(requestedPort);
+    t.is(port, false);
+
+    server.close();
+  });
+
+  server.listen(requestedPort, defaultHost);
 });
 
-test('should return `true` when port of requested host is free whereas the same port of the default host is occupied', async (t) => {
-  const requestedHost = '127.0.0.1';
-  const requestedPort = 3011;
-
+test.serial('should return first available port', wrapper, (t, done) => {
   const server = net.createServer();
-  await pify(server.listen.bind(server))(requestedPort, '0.0.0.0');
 
-  const port = await isPortFree(requestedPort, requestedHost);
+  server.once('listening', async () => {
+    server.once('close', done);
 
-  t.is(port, true);
+    const port = await findFreePort(requestedPort);
+    t.not(port, requestedPort);
+
+    server.close();
+  });
+
+  server.listen(requestedPort, defaultHost);
 });
+
+test.serial('should return `false` when occupied port of a requested host is chosen', wrapper, (t, done) => {
+  const server = net.createServer();
+
+  server.once('listening', async () => {
+    server.once('close', done);
+
+    const port = await isPortFree(requestedPort, requestedHost);
+    t.is(port, false);
+
+    server.close();
+  });
+
+  server.listen(requestedPort, requestedHost);
+});
+
+test.serial('should return `true` when port of requested host is free whereas the same port of the default host is occupied', wrapper, (t, done) => {
+  const server = net.createServer();
+
+  server.once('listening', async () => {
+    server.once('close', done);
+
+    const port = await isPortFree(requestedPort, requestedHost);
+    t.is(port, true);
+
+    server.close();
+  });
+
+  server.listen(requestedPort, defaultHost);
+});
+
