46579a3ba4bd81a83821b455c9feb31e7afefd4c
igoradamenko <mail@igoradamenko.com>
2020-07-24 19:22:54 +0300

    --wip--

--- test.js
+++ test.js
@@ -11,20 +11,25 @@ const server1 = net.createServer();
 server1.on('error', err => {
   console.log('server 1 error', err);
 });
-server1.listen(5000, '0.0.0.0');
+server1.listen(5000, '0.0.0.0', () => {
+  console.log('server 1', server1.address());
+});
 
 const server2 = net.createServer();
 server2.on('error', (err) => {
   console.log('server 2 error', err);
 });
-server2.listen(5000, '127.0.0.1');
+server2.listen(5000, '127.0.0.1', () => {
+  console.log('server 2', server2.address());
+});
 
 const server3 = net.createServer();
 server3.on('error', err => {
   console.log('server 3 error', err);
 });
-server3.listen(5000, '127.0.0.1');
-console.log('server3 listened');
+server3.listen(5000, '127.0.0.1', () => {
+  console.log('server 3', server3.address());
+});
 
 const wrapper = (t, run) => new Promise(resolve => run(t, resolve));
 
