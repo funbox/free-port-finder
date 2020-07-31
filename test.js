1f1808e261bd8ccc4138887e6e4993ee7446adbe
n.andreychenko <natandreychenko@gmail.com>
2020-07-31 17:27:45 +0300

    Update tests to simplify phrases

--- test.js
+++ test.js
@@ -9,37 +9,37 @@ const defaultHost = '0.0.0.0';
 
 const wrapper = (t, run) => new Promise(resolve => run(t, resolve));
 
-test.serial('should return the passed port value', async (t) => {
+test.serial('should return a port value when port if free', async (t) => {
   const port = await findFreePort(3000);
 
   t.is(port, 3000);
 });
 
-test.serial('should throw a port searching error when finding port outside the available range', async (t) => {
+test.serial('should throw a port searching error when port is found outside the available range', async (t) => {
   const port = await t.throwsAsync(() => findFreePort(65536));
 
   t.true(port.message.includes('should be >= 0 and < 65536. Received 65536.'));
 });
 
-test.serial('should return `true` when available port is chosen', async (t) => {
+test.serial('should return `true` when port is free', async (t) => {
   const isFree = await isPortFree(3007);
 
   t.is(isFree, true);
 });
 
-test.serial('should throw a port searching error when checking port outside the available range', async (t) => {
+test.serial('should throw a port searching error when port is outside the available range', async (t) => {
   const port = await t.throwsAsync(() => isPortFree(65536));
 
   t.true(port.message.includes('should be >= 0 and < 65536. Received 65536.'));
 });
 
-test.serial('should return the passed port value of the requested host', async (t) => {
+test.serial('should return a port value of the requested host', async (t) => {
   const port = await findFreePort(3000, '127.0.0.1');
 
   t.is(port, 3000);
 });
 
-test.serial('should return `false` when occupied port is chosen', wrapper, (t, done) => {
+test.serial('should return `false` when port is occupied', wrapper, (t, done) => {
   const server = net.createServer();
 
   server.once('listening', async () => {
@@ -69,7 +69,7 @@ test.serial('should return first available port', wrapper, (t, done) => {
   server.listen(requestedPort, defaultHost);
 });
 
-test.serial('should return `false` when occupied port of a requested host is chosen', wrapper, (t, done) => {
+test.serial('should return `false` when port of a requested host is occupied', wrapper, (t, done) => {
   const server = net.createServer();
 
   server.once('listening', async () => {
